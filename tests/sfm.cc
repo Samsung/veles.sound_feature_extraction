0ca78fbdcce841996b8139c774884f452bce1f51
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-04-16 11:30:12 +0400

    Added SFM test
    Change-Id: Id4e0c18c8309175ae55b919784c3410bdeb1aa03

---
+++ tests/sfm.cc
@@ -0,0 +1,45 @@
+/*! @file sfm.cc
+ *  @brief Spectral Flatness Measure calculation.
+ *  @author Markovtsev Vadim <v.markovtsev@samsung.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+#include <gtest/gtest.h>
+#include "src/transform_tree.h"
+#include "src/transform_registry.h"
+#include "src/formats/raw_format.h"
+#include "tests/speech_sample.inc"
+
+using SoundFeatureExtraction::TransformTree;
+using SoundFeatureExtraction::Formats::Raw16;
+using SoundFeatureExtraction::BuffersBase;
+
+TEST(MFCC, Calculation) {
+  ASSERT_NO_THROW({
+    TransformTree tt( { 48000, 16000 } );  // NOLINT(*)
+    tt.SetValidateAfterEachTransform(true);
+    tt.AddChain("SFM", { { "Window", "length=32" }, { "RDFT", "" },
+        { "ComplexMagnitude", "" }, { "Mean", "types=arithmetic geometric" },
+        { "SFM", "" } });
+    BuffersBase<Raw16> buffers(tt.RootFormat());
+    buffers.Initialize(1, 48000, 0);
+    memcpy(buffers[0]->Data.get(), data, sizeof(data));
+    tt.PrepareForExecution();
+    auto res = tt.Execute(buffers);
+    ASSERT_EQ(1, res.size());
+    res["SFM"]->Validate();
+    tt.Dump("/tmp/sfm.dot");
+    auto report = tt.ExecutionTimeReport();
+    for (auto r : report) {
+      printf("%s:\t%f\n", r.first.c_str(), r.second);
+    }
+  });
+}
+
+#include "tests/google/src/gtest_main.cc"
