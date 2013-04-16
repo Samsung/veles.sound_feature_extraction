ca89a9f146d65b8bd7605ec43dbed1b94d7615fa
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-04-16 09:38:24 +0400

    Added ArgMinMax transform (needed by dominant frequency component feature)
    Change-Id: I08db24e58956fbbb715302a2315c00d2bbf8506e

---
+++ src/transforms/argminmax.h
@@ -0,0 +1,60 @@
+/*! @file argminmax.h
+ *  @brief Arithmetic and geometric argminmaxs calculation.
+ *  @author Markovtsev Vadim <v.markovtsev@samsung.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+#ifndef SRC_TRANSFORMS_ARGMINMAX
+#define SRC_TRANSFORMS_ARGMINMAX
+
+#include <set>
+#include "src/formats/window_format.h"
+#include "src/formats/single_format.h"
+#include "src/transform_base.h"
+
+namespace SoundFeatureExtraction {
+namespace Transforms {
+
+typedef std::tuple<int, float> ArgMinMaxResult;
+
+class ArgMinMax
+    : public TransformBase<Formats::WindowFormatF,
+                           Formats::SingleFormat<ArgMinMaxResult>> {
+ public:
+  ArgMinMax();
+
+  TRANSFORM_INTRO("ArgMinMax", "Calculate argmin or argmax of the window.")
+
+  TRANSFORM_PARAMETERS(
+      TP("extremum", "Extremum type to find: min or max.",
+         kDefaultExtremum? "min" : "max")
+  )
+
+ protected:
+  virtual void InitializeBuffers(
+      const BuffersBase<Formats::WindowF>& in,
+      BuffersBase<ArgMinMaxResult>* buffers)
+  const noexcept;
+
+  virtual void Do(const BuffersBase<Formats::WindowF>& in,
+                  BuffersBase<ArgMinMaxResult> *out)
+  const noexcept;
+
+  static ArgMinMaxResult Do(bool simd, const float* input, size_t length,
+                            bool min) noexcept;
+
+private:
+  static const bool kDefaultExtremum;
+
+  bool extremum_;
+};
+
+}  // namespace Transforms
+}  // namespace SoundFeatureExtraction
+#endif  // SRC_TRANSFORMS_ARGMINMAX
---
+++ tests/transforms/argminmax.cc
@@ -0,0 +1,66 @@
+/*! @file complex_to_real.cc
+ *  @brief Tests for SoundFeatureExtraction::Transforms::ArgMinMax.
+ *  @author Shapichev Alexey <a.shapichev@samsung.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+
+#include <gtest/gtest.h>
+#include "src/transforms/argminmax.h"
+
+using SoundFeatureExtraction::Formats::WindowF;
+using SoundFeatureExtraction::Formats::WindowFormatF;
+using SoundFeatureExtraction::BuffersBase;
+using SoundFeatureExtraction::Transforms::ArgMinMax;
+using SoundFeatureExtraction::Transforms::ArgMinMaxResult;
+
+class ArgMinMaxTest
+    : public ArgMinMax, public testing::Test {
+ public:
+  BuffersBase<WindowF> Input;
+  BuffersBase<ArgMinMaxResult> Output;
+  int Size;
+
+  virtual void SetUp() {
+    Size = 486;
+    Input.Initialize(1, Size);
+    for (int i = 0; i < Size / 2; i++) {
+      Input[0]->Data.get()[i] = -i + 1;
+    }
+    for (int i = Size / 2; i < Size; i++) {
+      Input[0]->Data.get()[i] = Size - i + 1;
+    }
+    auto format = std::make_shared<WindowFormatF>(Size * 1000 / 18000, 18000);
+    SetInputFormat(format);
+    InitializeBuffers(Input, &Output);
+  }
+};
+
+#define EPSILON 0.15f
+
+#define ASSERT_EQF(a, b) ASSERT_NEAR(a, b, EPSILON)
+
+TEST_F(ArgMinMaxTest, Do) {
+  SetParameter("extremum", "max");
+  Do(Input, &Output);
+  ASSERT_EQ(Size / 2, std::get<0>(*Output[0]));
+  ASSERT_EQ(Size / 2 + 1, std::get<1>(*Output[0]));
+  SetParameter("extremum", "min");
+  Do(Input, &Output);
+  ASSERT_EQ(Size / 2 - 1, std::get<0>(*Output[0]));
+  ASSERT_EQ(- Size / 2 + 2, std::get<1>(*Output[0]));
+}
+
+#define EXTRA_PARAM true
+#define CLASS_NAME ArgMinMaxTest
+#define ITER_COUNT 500000
+#define NO_OUTPUT
+#include "tests/transforms/benchmark.inc"
+
+#include "tests/google/src/gtest_main.cc"
