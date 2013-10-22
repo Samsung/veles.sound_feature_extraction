5fc0aee67f0c9abcaaea554e2109be69f3a0b814
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-10-22 13:54:33 +0400

    Fixed bug with array formats in Python wrapper

--- python/sound_feature_extraction/formatters.py
+++ python/sound_feature_extraction/formatters.py
@@ -22,6 +22,8 @@ class Formatters(object):
     @staticmethod
     def parse(array, format_name):
         if format_name.find("<") == -1:
+            if format_name.endswith("*"):
+                format_name = format_name[:-1]
             conv_array = Formatters.reinterpret_cast(array, format_name)
             if conv_array.size > 1:
                 return conv_array
