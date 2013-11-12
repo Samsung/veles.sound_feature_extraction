d97839cd23e70ea3489bf0f6955fca85127df601
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-12 11:03:18 +0400

    Removed files from style check which were extracted to libSimd
    Change-Id: I4858934f672e4a2a56e27fce8279c60ea514afa8

--- fullcheck_xml.sh
+++ fullcheck_xml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./fftf/*" -not -path "./src/boost/*" -not -name "config.h" -not -path "./src/primitives/avx_mathfun.h" -not -path "./inc/boost/*" -not -path "./src/primitives/neon_mathfun.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
+find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./fftf/*" -not -path "./src/boost/*" -not -name "config.h" -not -path "./inc/boost/*" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
