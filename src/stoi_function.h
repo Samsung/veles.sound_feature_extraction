aec7e9aa491570c6c1735b764c3debf20182c5e9
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-15 14:45:50 +0400

    Added std::function-ized std::stoi
    Change-Id: Ide957f27efe74be22ec0c5f0b77cce8801dd9c45

---
+++ src/stoi_function.h
@@ -0,0 +1,27 @@
+/*! @file stoi_function.h
+ *  @brief std::transform friendly std::stoi wrapper.
+ *  @author Vadim Markovtsev <v.markovtsev@samsung.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+#ifndef STOI_FUNCTION_H_
+#define STOI_FUNCTION_H_
+
+#include <functional>
+#include <string>
+
+namespace std {
+
+static const auto stoi_function = bind(
+    static_cast<int(*)(const string&, size_t*, int)>(&stoi),
+    placeholders::_1, nullptr, 10);
+
+}  // namespace std
+
+#endif  // STOI_FUNCTION_H_
