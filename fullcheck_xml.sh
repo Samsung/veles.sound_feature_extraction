0cb6260cc4e23df9cfb342267e2dec5474eae226
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-18 18:29:58 +0400

    Updated fullcheck_xml.sh
    Change-Id: Ie2fbe48e64b0080afbbc8ee3f346140a1eacc5eb

--- fullcheck_xml.sh
+++ fullcheck_xml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./fftf/*" -not -path "./src/boost/*" -not -name "config.h" -not -path "./inc/boost/*" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
+find \( -name "*.cc" -o -name "*.h" \) -not -path "./build*" -not -path "./tests/google/*" -not -path "./tests/transforms/convolve.cc" -not -path "./python/*" -not -path "./fftf/*" -not -path "./simd/*" -not -path "*/DSPFilters/*" -not -path "./src/boost/*" -not -name "config.h" -not -path "./inc/boost/*" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
